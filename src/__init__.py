5ef71dcc7f024db807913a3fb65f4153c818cd83
uyenbhku <uyen.bhku@gmail.com>
2024-06-14 09:58:35 +0700

    new feature: cloudinary upload

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,9 @@
 from flask import Flask, flash, render_template, request, redirect, url_for, send_from_directory, jsonify
+from flask_cors import CORS, cross_origin
+from flask_restful import Resource, Api 
+from werkzeug.datastructures import FileStorage 
+import cloudinary 
+from cloudinary.uploader import upload as cloudinary_upload
 from .utils import allowed_file
 import os
 from dotenv import load_dotenv
@@ -7,6 +12,18 @@ load_dotenv()
 
 # Khởi tạo và cài đặt Flask
 app = Flask(__name__)
+api = Api(app) 
+# Cho phép tất cả API được vượt tường
+CORS(app)
+
+
+# Cấu hình Cloudinary 
+cloudinary.config.update = ({
+    'cloud_name':os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'api_key': os.getenv('CLOUDINARY_API_KEY'),
+    'api_secret': os.getenv('CLOUDINARY_API_SECRET')
+})
+
 
 # Cấu hình thư mục upload bên ngoài thư mục static
 UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER')
@@ -31,13 +48,17 @@ def upload():
             return redirect(request.url)
         
         if file and allowed_file(file.filename):
+            app.logger.info('%s file_to_upload', file)
+            upload_result = cloudinary_upload(
+                file, 
+                resource_type='image',
+                folder=UPLOAD_FOLDER
+            )
             filename = file.filename
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
             return redirect(url_for(
                 'result', 
                 filename=filename, 
-                # url=upload_result['url']
+                url=upload_result['url']
             ))
         else:
             flash("File type not allowed!", "error")
@@ -53,4 +74,5 @@ def uploaded_file(filename):
 @app.route('/result')
 def result():
     filename = request.args.get('filename')
-    return render_template('result.html', filename=filename)
+    url = request.args.get('url')
+    return render_template('result.html', filename=filename, url=url)

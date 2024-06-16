9ee865f6197e71cb3a6663245f89f8639fc6e0d1
uyenbhku <uyen.bhku@gmail.com>
2024-06-16 18:30:10 +0700

    add code to load Mai's model

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 import re
+import tensorflow as tf
+
 
 def get_url_images_in_text(text:str):
     '''finds image urls'''
@@ -8,3 +10,13 @@ def get_url_images_in_text(text:str):
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+class FixedDropout(tf.keras.layers.Dropout):
+    def _get_noise_shape(self, inputs):
+        if self.noise_shape is None:
+            return self.noise_shape
+
+        symbolic_shape = tf.keras.backend.shape(inputs)
+        noise_shape = [symbolic_shape[axis] if shape is None else shape
+                        for axis, shape in enumerate(self.noise_shape)]
+        return tuple(noise_shape)

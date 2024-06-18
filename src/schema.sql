afc38ccb38aa213c187e90935f56d082c8312135
uyenbhku <uyen.bhku@gmail.com>
2024-06-18 11:30:03 +0700

    add new schema

--- src/schema.sql
+++ src/schema.sql
@@ -1,9 +1,17 @@
 DROP TABLE IF EXISTS PlantDisease;
 
 CREATE TABLE PlantDisease (
-  disease_id TEXT PRIMARY KEY NOT NULL UNIQUE,
-  plant_name TEXT NOT NULL,
-  disease_name TEXT NOT NULL,
-  affect TEXT,
-  solution TEXT
+	disease_id	TEXT NOT NULL UNIQUE,
+	plant_name	TEXT NOT NULL,
+	disease_name	TEXT NOT NULL,
+	affect	TEXT,
+	solution	TEXT,
+	PRIMARY KEY(disease_id)
+);
+
+CREATE TABLE PredictionLog (
+	image_url	TEXT NOT NULL UNIQUE,
+	predict_result	TEXT NOT NULL,
+	model_name	TEXT,
+	PRIMARY KEY(image_url)
 );
